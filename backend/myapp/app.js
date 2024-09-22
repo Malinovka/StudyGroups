dc5296515fbbed6333e0e508b8a76309d8bed0cc
drizzle846 <khushisharma.4804@gmail.com>
2024-09-22 13:16:48 -0700

    added get all users

--- backend/myapp/app.js
+++ backend/myapp/app.js
@@ -7,6 +7,22 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+app.get("/api/users", (req, res, next) => {
+    var sql = "SELECT * FROM user"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({"error":err.message});
+            return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+    });
+});
+
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
