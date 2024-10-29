ae5d6edb289b020c175d93b621c1337e82fa8998
drizzle846 <khushisharma.4804@gmail.com>
2024-10-28 22:20:58 -0700

    Chatbox frontend

--- frontend/my-app/src/app/page.js
+++ frontend/my-app/src/app/page.js
@@ -1,10 +1,64 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
+import React, { useRef, useState } from 'react';
+import Editor from './Editor';
+import {Quill} from "react-quill";
+import './styles.css'; // Adjust the path if needed
 
-export default function Home() {
-  return (
-    <div>
-      <h1>Hello</h1>
-    </div>
-  );
-}
+const Delta = Quill.import('delta');
+
+const App = () => {
+    const [range, setRange] = useState();
+    const [lastChange, setLastChange] = useState();
+    const [readOnly, setReadOnly] = useState(false);
+
+    // Use a ref to access the quill instance directly
+    const quillRef = useRef();
+
+    return (
+        <div>
+            <Editor
+                ref={quillRef}
+                readOnly={readOnly}
+                defaultValue={new Delta()
+                    .insert('Hello')
+                    .insert('\n', { header: 1 })
+                    .insert('Some ')
+                    .insert('initial', { bold: true })
+                    .insert(' ')
+                    .insert('content', { underline: true })
+                    .insert('\n')}
+                onSelectionChange={setRange}
+                onTextChange={setLastChange}
+            />
+            <div className="controls">
+                <label>
+                    Read Only:{' '}
+                    <input
+                        type="checkbox"
+                        value={readOnly}
+                        onChange={(e) => setReadOnly(e.target.checked)}
+                    />
+                </label>
+                <button
+                    className="controls-right"
+                    type="button"
+                    onClick={() => {
+                        alert(quillRef.current?.getLength());
+                    }}
+                >
+                    Get Content Length
+                </button>
+            </div>
+            <div className="state">
+                <div className="state-title">Current Range:</div>
+                {range ? JSON.stringify(range) : 'Empty'}
+            </div>
+            <div className="state">
+                <div className="state-title">Last Change:</div>
+                {lastChange ? JSON.stringify(lastChange.ops) : 'Empty'}
+            </div>
+        </div>
+    );
+};
+
+export default App;
