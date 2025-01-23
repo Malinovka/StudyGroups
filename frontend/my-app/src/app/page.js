2578a63ee063be465e0af0ab17cedf35023f8bb9
enora <enorasun1120@gmail.com>
2025-01-22 21:26:02 -0800

    added GroupSelector and modified page.js to show GroupSelector

--- frontend/my-app/src/app/page.js
+++ frontend/my-app/src/app/page.js
@@ -1,6 +1,7 @@
 'use client';
 import React, { useState } from 'react';
 import ChatBox from './chatbox';
+import GroupList from './GroupSelector';
 import './styles.css';
 
 const App = () => {
@@ -13,13 +14,37 @@ const App = () => {
         ]);
     };
 
+    const groups = [
+        { name: 'Group 1' },
+        { name: 'Group 2' },
+        { name: 'Group 3' },
+        { name: 'Group 4' },
+        { name: 'Group 5' },
+        { name: 'Group 6' },
+        { name: 'Group 7' },
+        { name: 'Group 8' },
+        { name: 'Group 9' },
+        { name: 'Group 10' }
+      ];
+    
+      const handleGroupSelect = (group) => {
+        console.log('Selected Group:', group);
+      };
+
     return (
+        <div className="page-container">
         <div className="page">
             <div className="main">
                 <ChatBox messages={messages} onSendMessage={handleSendMessage} />
             </div>
         </div>
+        <div>
+        <h1>Select a Group</h1>
+        <GroupList groups={groups} onGroupSelect={handleGroupSelect} />
+      </div>
+      </div>
     );
+    
 };
 
 export default App;
